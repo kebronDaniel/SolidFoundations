98ef1e32b964845876c0c3ecd554d1d2332c7f08
KebronDaniel <kebronjrdaniel@gmail.com>
2026-05-06 13:26:53 +0200

    heapAndStack

--- src/main/java/org/spring/solidfoundations/coreJava/RandD.java
+++ src/main/java/org/spring/solidfoundations/coreJava/RandD.java
@@ -7,5 +7,8 @@ public class RandD {
         System.out.println(p.getName());
 
         // p holds the copy of the reference thus it simply changes the reference to a new one.
+
+        // Also the jvm stores a stack frame meaning a dedicated place in the stack to store all the things that the method needs.
+        // this is used when this method is called.
     }
 }
