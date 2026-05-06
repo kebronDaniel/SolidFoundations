f382264541e0952d3b15c1f95e8e9b2a898fdbd4
KebronDaniel <kebronjrdaniel@gmail.com>
2026-05-06 12:28:48 +0200

    cleaned main

---
+++ src/main/java/org/spring/solidfoundations/SolidFoundationsApplication.java
@@ -0,0 +1,13 @@
+package org.spring.solidfoundations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidFoundationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidFoundationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/solidfoundations/SoildFoundationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.solidfoundations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoildFoundationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
