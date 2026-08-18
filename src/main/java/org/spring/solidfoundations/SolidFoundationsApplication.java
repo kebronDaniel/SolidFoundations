6bfa2f862d0beeef07ed5870e5f2b6de40fb52a8
KebronDaniel <kebronjrdaniel@gmail.com>
2026-08-18 19:45:51 +0200

    constructor subset

--- src/main/java/org/spring/solidfoundations/SolidFoundationsApplication.java
+++ src/main/java/org/spring/solidfoundations/SolidFoundationsApplication.java
@@ -1,8 +1,5 @@
 package org.spring.solidfoundations;
 
-import org.spring.solidfoundations.coreJava.oop.staticMembers.ChildEmployee;
-import org.spring.solidfoundations.coreJava.oop.staticMembers.Employee;
-import org.spring.solidfoundations.coreJava.oop.staticMembers.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,15 +9,6 @@ public class SolidFoundationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SolidFoundationsApplication.class, args);
 
-        Employee employee = new Employee("first employee");
-
-        // here the parents static method overrides the child's(one static member per class)
-        // because its the declared type that is taken.
-        // its called method hiding.
-        Employee employee2 = new ChildEmployee("second employee");
-
-        Employee.show(employee);
-        ChildEmployee.show(employee2);
 
     }
 
---
+++ src/main/java/org/spring/solidfoundations/coreJava/oop/constructor/Product.java
@@ -0,0 +1,16 @@
+package org.spring.solidfoundations.coreJava.oop.constructor;
+
+public class Product {
+    private final String name;
+    private final String type;
+
+    public Product(String name) {
+        // use the second constructor to generate.
+        this(name,"default type");
+    }
+
+    public Product(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
